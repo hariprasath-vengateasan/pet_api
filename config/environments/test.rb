f3f6bd9647c82562e09298532d4c307ce7ce8e2e
Hari Prasath Vengateasan <hariven1998@gmail.com>
2025-07-10 13:20:25 +0400

    correction in test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,9 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.hosts = [
+    "example.com",     # Allow requests from example.com
+    /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  ]
 end
